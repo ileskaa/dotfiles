38fe013d45896c0b025265ba86f332cb4c39efa0
ileskaa <akseli.ingervo@gmail.com>
2026-04-19 18:35:53 +0300

    nvim, insert mode: remove jk => <Esc> binding

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -5,7 +5,6 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 map("n", "K", function() vim.lsp.buf.hover { border = "rounded" } end, { desc = "LSP hover" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
